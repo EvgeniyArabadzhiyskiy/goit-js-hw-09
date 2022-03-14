74164e80a834e2812600c0d7871ca075a4c7d5cc
EVGENIY-12 <pet3094@gmail.com>
2022-03-14 21:31:03 +0200

    Add 01-color.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
 
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+}
+
+
+const makeBgColor = {
+    intervalId: null,
+
+    start() {
+        this.intervalId = setInterval(getRandomHexColor, 1000);
+        refs.startBtn.disabled = true;
+        
+        
+    },
+
+    stop() {
+        clearInterval(this.intervalId);
+        refs.startBtn.disabled = false;
+        
+    },
+    
+}
+
+refs.startBtn.addEventListener('click', makeBgColor.start.bind(makeBgColor));
+refs.stopBtn.addEventListener('click', makeBgColor.stop.bind(makeBgColor));
+
+function getRandomHexColor() {
+    const bgColor =  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    console.log(bgColor);
+    document.body.style.backgroundColor = bgColor;
+
+}
