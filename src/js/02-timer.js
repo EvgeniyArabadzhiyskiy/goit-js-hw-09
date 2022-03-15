152150a930157a51249c56e13248225e572dd052
EVGENIY-12 <pet3094@gmail.com>
2022-03-15 23:51:19 +0200

    Add files

---
+++ src/js/02-timer.js
@@ -0,0 +1,107 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import '../css/02-timer.css'
+
+
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  spanDays: document.querySelector('span[data-days]'),
+  spanHours: document.querySelector('span[data-hours]'),
+  spanMinutes: document.querySelector('span[data-minutes]'),
+  spanSeconds: document.querySelector('span[data-seconds]'),
+}
+
+refs.startBtn.disabled = true;
+
+const options = {
+  
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onChange(selectedDates) { 
+    const time = selectedDates[0] - Date.now()
+    
+    if (time > 0) {
+      refs.startBtn.disabled = false;
+
+    }
+    else {refs.startBtn.disabled = true;}
+
+  },
+  
+  onClose(selectedDates) {
+    const time = selectedDates[0] - Date.now()
+    
+    if (time > 0) {
+      const date1 = Date.now()
+
+      refs.startBtn.addEventListener('click', onClick);
+
+      function onClick() {
+        refs.startBtn.disabled = true;
+      
+        const intervalID = setInterval(() => {
+          
+          const date2 = Date.now()
+          const deltaTime = date2 - date1;
+          const getTime = time - deltaTime;
+         
+          const getConvertTime = convertMs(getTime)
+          console.log(convertMs(getTime));
+
+          updateTimerFace(getConvertTime)
+
+          function updateTimerFace({ days, hours, minutes, seconds }) {
+            refs.spanDays.textContent = days;
+            refs.spanHours.textContent = hours;
+            refs.spanMinutes.textContent = minutes;
+            refs.spanSeconds.textContent = seconds;
+
+          }
+
+          if (getTime < 1000) {
+            clearInterval(intervalID);
+            
+          }
+
+        },1000)
+  
+      }
+      
+      refs.startBtn.disabled = false;
+      
+    }
+    else {alert("Please choose a date in the future")}
+    
+  },
+};
+
+
+const timer = flatpickr("#datetime-picker", options)
+
+
+function convertMs(ms) {
+  
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+ 
+  const days = pad(Math.floor(ms / day));
+  const hours = pad(Math.floor((ms % day) / hour));
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+  
+  return { days, hours, minutes, seconds };
+  
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+
+
+
