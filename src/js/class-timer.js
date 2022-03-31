71006695942de72f96d98fefd16bd40446e62f4f
EVGENIY-12 <pet3094@gmail.com>
2022-03-31 16:04:55 +0300

    Create class-timer.js

---
+++ src/js/class-timer.js
@@ -0,0 +1,42 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+export class Timer {
+  constructor({ onTick, onConvert, onDisebled } = {}) {
+    this.selectedData = 0;
+    this.convertDate = {};
+    this.isActive = false;
+    this.onTick = onTick;
+    this.onConvert = onConvert;
+    this.onDisebled = onDisebled;
+  }
+
+  getCountdownTime() {
+    return this.selectedData - Date.now();
+  }
+
+  choosesDates(selectedDates) {
+    if (selectedDates[0] > Date.now() && !this.isActive) {
+      this.isActive = true;
+      this.onDisebled(false);
+    }
+    if (selectedDates[0] < Date.now()) {
+      this.onDisebled(true);
+      Notify.failure('Please choose a date in the future');
+    }
+  }
+
+  turnsOnTimer(deltaTime) {
+    const intervalId = setInterval(() => {
+      deltaTime -= 1000;
+      this.convertDate = this.onConvert(deltaTime);
+      this.onTick(this.convertDate);
+
+      console.log(this.convertDate);
+
+      if (deltaTime < 1000) {
+        z;
+        clearInterval(intervalId);
+        this.isActive = false;
+      }
+    }, 1000);
+  }
+}
